148a3045ad6574af2969599eda3fe334f336ff06
daviti chivadze <davidchivadze96@gmail.com>
2020-03-05 10:34:22 +0400

    add Copy Button

--- src/app/Components/create-signature/create-signature.component.ts
+++ src/app/Components/create-signature/create-signature.component.ts
@@ -18,8 +18,41 @@ status;
     this.SignatureModel.OfficePhone=2922222;
     
   }
-  copyClipBoard(){
+  createSignature(FirstName,LastName:string,Postition,Sector,Department,City,OfficeAddress,Email,OfficePhone,InternalPhone,MobileNumber,ImageURL){
+    return `<b><p  style="color:#808080;font-weight:bold;" >${FirstName}<label>&nbsp; ${LastName}</label><p></p></b><br><br>
+    <p style="color:#808080;" >${Postition}</p>
+    <p style="color:#808080;" >${Sector}</p>
+    <p  style="color:#808080;" >${Department}</p>
+    <p  style="color:#808080;">${City},<span>${{OfficeAddress}}</span></p>
+    <p style="color:#808080;">E-Mail:<span >${Email}</span></p>
+    <p  style="color:#808080;">Phone: (+995) ${OfficePhone} (ext. ${InternalPhone})</p>
+    <p  style="color:#808080;">Mobile: ${MobileNumber}</p>
+    <p><img src="${ImageURL}"></p>
+    `
+  }
+  copyClipboard(){
+var image=this.divView.nativeElement.src;
 
+    var str=this.createSignature(this.SignatureModel.FirstName,this.SignatureModel.LastName,this.SignatureModel.Postition,this.SignatureModel.Sector,this.SignatureModel.Department,this.SignatureModel.City,this.SignatureModel.OfficeAddress,this.SignatureModel.Email,this.SignatureModel.OfficePhone,this.SignatureModel.InternalPhone,this.SignatureModel.MobileNumber,image);
+    function listener(e) { 
+      e.clipboardData.setData("text/html", str);
+      e.clipboardData.setData("text/plain", str);
+      e.preventDefault();
+    }
+    document.addEventListener("copy", listener);
+    document.execCommand("copy");
+    document.removeEventListener("copy", listener);
+  }
+  changeOfficeAddress(){
+    if(this.SignatureModel.IsHeadOffice){
+    this.SignatureModel.OfficeAddress="Ana Politkovskaia Street 9a"
+    this.SignatureModel.City="Tbilisi"
+    this.SignatureModel.OfficePhone=2922222
+  }else{
+    this.SignatureModel.OfficeAddress=undefined;
+    this.SignatureModel.City=undefined;
+    this.SignatureModel.OfficePhone=undefined;
+  }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { CreateSignatureComponent } from './Components/create-signature/create-signature.component'
+import {NgxEditorModule} from 'ngx-editor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +15,9 @@ import { CreateSignatureComponent } from './Components/create-signature/create-s
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgxEditorModule,
+    BrowserAnimationsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
